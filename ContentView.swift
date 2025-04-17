94d99013ae9493f96a3e3772a0823ef6eccc7a86
Khadija <kazam8513@stu.d214.org>
2025-04-17 13:34:39 -0500

    added background and animation of the bg

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var xPosition:CGFloat = -330
+    @State var yPosition:CGFloat = 0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            Image(systemName: "photo.artframe")
+            
+                .resizable()
+                .frame(width: 1000,height: 1000)
+                .offset(x: xPosition, y: yPosition)
+                .onAppear{
+                    withAnimation(.easeIn.speed(0.1)){
+                        xPosition += 500
+                        
+                        if xPosition == 170{
+                            withAnimation(.easeOut.speed(0.1).delay(5)){
+                                xPosition += -500
+                    }
+                }
+           
+                }
+            }
+            
         }
     }
 }
