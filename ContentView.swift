2f1990be485efb3bc23d5109ba5af85053d865de
Khadija <kazam8513@stu.d214.org>
2025-04-22 12:40:13 -0500

    nothing changed

--- ContentView.swift
+++ ContentView.swift
@@ -3,8 +3,10 @@ import SwiftUI
 struct ContentView: View {
     @State var xPosition:CGFloat = -330
     @State var yPosition:CGFloat = 0
+    
     var body: some View {
         VStack {
+            
             
             Image(systemName: "photo.artframe")
             
@@ -12,12 +14,14 @@ struct ContentView: View {
                 .frame(width: 1000,height: 1000)
                 .offset(x: xPosition, y: yPosition)
                 .onAppear{
+                    
                     withAnimation(.easeIn.speed(0.1)){
                         xPosition += 500
                         
                         if xPosition == 170{
                             withAnimation(.easeOut.speed(0.1).delay(5)){
                                 xPosition += -500
+                                
                     }
                 }
            
