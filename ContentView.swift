24c8c4b454be97cce537c7010e5bd54c8cf74ec8
lmakula6243 <lmakula6243@stu.d214.org>
2025-04-24 13:33:17 -0500

    made the background move back and forth

--- ContentView.swift
+++ ContentView.swift
@@ -1,52 +1,34 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var xPosition:CGFloat = -550
-    @State var yPosition:CGFloat = 0
+    @State var xPosition: CGFloat = -550
+    @State var yPosition: CGFloat = 0
     
     var body: some View {
-        NavigationView{
-        ZStack {
-            
-            
-            
-            Image("BG")
-            
-            
-            
-                .resizable()
-                .frame(width: 1500,height: 1000)
-                .offset(x: xPosition, y: yPosition)
-                .onAppear{
-                    withAnimation(.easeIn.speed(0.1).repeatForever()){
-                        xPosition += 1100
-                        
-                        if xPosition == 550{
-                            withAnimation(.easeInOut.speed(-5).repeatForever()){
-                                xPosition += -1100
-                                if xPosition == -550 {
-                                    withAnimation(.easeInOut.speed(-5).repeatForever()){
-                                        xPosition += 1100
-                                        
-                                    }
-                                    
-                                    
-                                    
-                                }
-                                
-                            }
-                            
+        NavigationView {
+            ZStack {
+                Image("BG")
+                    .resizable()
+                    .frame(width: 1500, height: 1000)
+                    .offset(x: xPosition, y: yPosition)
+                    .onAppear {
+                        print(xPosition)
+
+                        withAnimation(Animation.easeInOut(duration: 10).repeatForever(autoreverses: true)) {
+                            // auto reverse: -550 ➡️ 550 ➡️ -550 ➡️ 550... (looping)
+                            xPosition = 550
                         }
-                        
+                    }
+                    .onDisappear {
+                        xPosition = 550
+                        print(xPosition)
                     }
                     
-                }
-           
-                VStack{
+                
+                VStack {
                     Image("title3")
                         .resizable()
                         .frame(width: 400, height: 350)
-                    
                     
                     NavigationLink {
                         EventsPageView()
@@ -55,13 +37,12 @@ struct ContentView: View {
                             .resizable()
                             .frame(width: 200, height: 120)
                     }
-                    
                 }
-            
-                    }
-                
-                
             }
+            
         }
+        
     }
+}
+
 
